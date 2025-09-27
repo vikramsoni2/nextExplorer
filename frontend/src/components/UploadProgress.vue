2df2b236b722421294b358def068fa1380d97df2
Vikram <vikram9880@gmail.com>
2025-09-28 00:20:38 +0200

    added file progress object example

--- frontend/src/components/UploadProgress.vue
+++ frontend/src/components/UploadProgress.vue
@@ -14,6 +14,65 @@ const { x, y, style } = useDraggable(el, {
   preventDefault: true,
 })
 
+
+/*
+uppyStore.state.files looks like this 
+[
+  {"uppy-img/9501/mp4-2v-1e-video/mp4-img/9501/mp4-2v-1e-21594174-1758034388670":{
+    "source":"",
+    "id":"uppy-img/9501/mp4-2v-1e-video/mp4-img/9501/mp4-2v-1e-21594174-1758034388670",
+    "name":"IMG_9501.mp4",
+    "extension":"mp4",
+    "meta":{
+      "relativePath":"IMG_9501.mp4",
+      "name":"IMG_9501.mp4",
+      "type":"video/mp4",
+      "uploadTo":"Projects"
+    },
+    "type":"video/mp4",
+    "data":"[object File]",
+    "progress":{
+      "uploadStarted":1759011067795,
+      "uploadComplete":false,
+      "percentage":21,
+      "bytesUploaded":4620158.343919781,
+      "bytesTotal":21594174
+    },
+    "size":21594174,
+    "isGhost":false,
+    "isRemote":false,
+    "remote":"",
+    "preview":"__vue_devtool_undefined__"
+  },
+  "uppy-img/9501/mov-2v-1e-video/quicktime-img/9501/mov-2v-1e-86916075-1758034262331":{
+    "source":"",
+    "id":"uppy-img/9501/mov-2v-1e-video/quicktime-img/9501/mov-2v-1e-86916075-1758034262331",
+    "name":"IMG_9501.MOV",
+    "extension":"MOV",
+    "meta":{
+      "relativePath":"IMG_9501.MOV",
+      "name":"IMG_9501.MOV",
+      "type":"video/quicktime",
+      "uploadTo":"Projects"
+    },
+    "type":"video/quicktime",
+    "data":"[object File]",
+    "progress":{
+      "uploadStarted":1759011133463,
+      "uploadComplete":false,
+      "percentage":2,
+      "bytesUploaded":1327094.5639498732,
+      "bytesTotal":86916075
+    },
+    "size":86916075,
+    "isGhost":false,
+    "isRemote":false,
+    "remote":"",
+    "preview":"__vue_devtool_undefined__"}
+  }
+]
+*/
+
 const progress = computed(() => uppyStore.state.totalProgress)
 
 const uploadInProgress = computed(() => Object.keys(uppyStore.state.currentUploads).length > 0)
@@ -26,6 +85,7 @@ const uploadedBytes = computed(() => Object.values(uppyStore.state.files)
     .filter(file => !file.progress.uploadComplete) // Filter files where upload isn't complete
     .reduce((acc, file) => acc + file.progress.bytesUploaded, 0)
 )
+
 
 
 </script>
