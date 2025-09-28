2b5f0d62817a28c62f5fac4c60fe0def27a5dc17
Vikram <vikram9880@gmail.com>
2025-09-29 01:46:59 +0200

    text-icon update

--- frontend/src/icons/files/txt-icon.vue
+++ frontend/src/icons/files/txt-icon.vue
@@ -1,12 +1,12 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
 
         <path fill="#fff" d="M6.5 2h13.6l6.4 6.4V28.5c0 .8-.7 1.5-1.5 1.5h-18c-.8 0-1.5-.7-1.5-1.5v-25c0-.8.7-1.5 1.5-1.5z"/>
         <path fill="#fff" d="M20 2v6c0 .6.4 1 1 1h6L20 2z"/>
 
         <path fill="currentColor" d="M9 14h14v1H9zm0 3h14v1H9zm0 3h14v1H9zm0 3h14v1H9zm0 3h14v1H9z"/>
-    </svg> 
-<!-- 
+    </svg>  -->
+
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true">
 
   <path fill="#FFFFFF" stroke="#D1D5DB" stroke-width="1" d="M6 4h14l6 6v18a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"/>
@@ -16,7 +16,7 @@
   <rect x="9" y="14" width="14" height="2" rx="1" fill="#9CA3AF"/>
   <rect x="9" y="18" width="12" height="2" rx="1" fill="#9CA3AF"/>
   <rect x="9" y="22" width="10" height="2" rx="1" fill="#9CA3AF"/>
-</svg> -->
+</svg> 
 
 
 </template>
